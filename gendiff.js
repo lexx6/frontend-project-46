9f3dc0dce4cb2bbeb3f707e4c4cd00630475a802
lexx696 <lexx696@yandex.ru>
2024-02-08 13:32:07 +0300

    add file1, file2, fix gendiff, package.json

--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,7 @@
 import { Command } from 'commander';
+import { cwd } from 'node:process';
+import path from 'node:path'; 
+
 const program = new Command();
 
 program 
@@ -6,6 +9,11 @@ program
     .version('1.0.0')
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format [type]', 'output format')
-    .helpOption('-h, --help', 'output usage information');
+    .helpOption('-h, --help', 'output usage information')
+    .action((filepath1, filepath2, {format}) => {
+        const file1 = path.resolve(cwd(), filepath1); //path.extname()
+        const file2 = path.resolve(cwd(), filepath2);
+        console.log(file1, file2, format);
+    });
 
 program.parse(process.argv);
