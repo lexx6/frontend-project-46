77681e456182199a3dddd284ca5a1c79c248a95a
lexx696 <lexx696@yandex.ru>
2024-02-08 12:44:30 +0300

    add files

---
+++ gendiff.js
@@ -0,0 +1,11 @@
+import { Command } from 'commander';
+const program = new Command();
+
+program 
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
+    .helpOption('-h, --help', 'output usage information');
+
+program.parse(process.argv);
