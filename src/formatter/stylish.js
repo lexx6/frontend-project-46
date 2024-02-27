50ab36ea1efe90a959331412093207335d691cb0
lexx696 <lexx696@yandex.ru>
2024-02-27 16:31:08 +0300

    stylish format

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,9 +1,48 @@
-export default (diff) => `{\n${diff.map((e) => {
-  switch (e.state) {
-    case 'shared': return `    ${e.key}: ${e.oldValue}`;
-    case 'deleted': return `  - ${e.key}: ${e.oldValue}`;
-    case 'added': return `  + ${e.key}: ${e.newValue}`;
-    case 'updated': return `  - ${e.key}: ${e.oldValue}\n  + ${e.key}: ${e.newValue}`;
-    default: throw new Error(`Wrong format: \n${JSON.stringify(diff)}`);
+// получает массив необработанных объектов, возвращает массив обработанных string
+const getNodesRecursive = (nodeList, depth = 0, symbolDepth = null) => nodeList.map((node) => { //
+  // функция обрабатывает значение ноды и возвращает просто стринг, либо стринг в виде объекта
+  const getStringFormatted = (symbol, key, value, dep, symDepth = null) => {
+    const defaultOffset = 2;
+    const levelOffset = 4;
+    const offset = ' '.repeat(levelOffset * dep + defaultOffset);
+    const sym = symDepth !== null && dep > symDepth ? ' ' : symbol;
+    const sibling = (val) => `{\n${val}${offset}  }\n`;
+    return Array.isArray(value)
+      ? `${offset}${sym} ${key}: ${sibling(getNodesRecursive(value, dep + 1, symDepth).join(''))}`
+      : `${offset}${sym} ${key}: ${value}\n`;
+  };
+
+  switch (node.state) {
+    case ('shared'): {
+      return Array.isArray(node.children)
+        ? getStringFormatted(' ', node.key, node.children, depth, symbolDepth)
+        : getStringFormatted(' ', node.key, node.oldVal, depth, symbolDepth);
+    }
+    case ('deleted'): {
+      return Array.isArray(node.children)
+        ? getStringFormatted('-', node.key, node.children, depth, symbolDepth ?? depth)
+        : getStringFormatted('-', node.key, node.oldVal, depth, symbolDepth);
+    }
+    case ('updated'): {
+      return [
+        Object.hasOwn(node, 'oldVal')
+          ? getStringFormatted('-', node.key, node.oldVal, depth, symbolDepth)
+          : getStringFormatted('-', node.key, node.children, depth, symbolDepth ?? depth),
+        Object.hasOwn(node, 'newVal')
+          ? getStringFormatted('+', node.key, node.newVal, depth, symbolDepth)
+          : getStringFormatted('+', node.key, node.children, depth, symbolDepth),
+      ].join('');
+    }
+    case ('added'): {
+      return Array.isArray(node.children)
+        ? getStringFormatted('+', node.key, node.children, depth, symbolDepth ?? depth)
+        : getStringFormatted('+', node.key, node.newVal, depth, symbolDepth);
+    }
+    default: throw new Error(`Wrong state: \n${JSON.stringify(node)}`);
   }
-}).join('\n')}\n}`;
+});
+
+export default (diff) => {
+  const nodes = getNodesRecursive(diff).join('');
+  return `{\n${nodes}}`;
+};
