7cbe3e5dd6f00eabadeacfa511dbb95627972489
lexx696 <lexx696@yandex.ru>
2024-04-24 10:00:06 +0300

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,12 +10,12 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (name) => fs.readFileSync(getFixturePath(name), 'utf-8');
 
 const cases = [
-  // [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('JsonTest.txt'), 'json'],
-  // [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('PlainTest.txt'), 'plain'],
+  [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('JsonTest.txt'), 'json'],
+  [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('PlainTest.txt'), 'plain'],
   [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('StylishTest.txt'), 'stylish'],
-  // [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFile('JsonTest.txt'), 'json'],
-  // [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFile('PlainTest.txt'), 'plain'],
-  // [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFile('StylishTest.txt'), 'stylish'],
+  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFile('JsonTest.txt'), 'json'],
+  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFile('PlainTest.txt'), 'plain'],
+  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), readFile('StylishTest.txt'), 'stylish'],
 ];
 
 test.each(cases)('diff beetween %s and %s', (file1, file2, result, format) => {
--- src/formatter/plain.js
+++ src/formatter/plain.js
@@ -1,49 +1,26 @@
-const getValueString = (node, key) => {
-  if (Array.isArray(node[key])) return '[complex value]';
-  if (typeof node[key] === 'string') return `'${node[key]}'`;
-  if (typeof node[key] === 'boolean') return node[key];
-  if (typeof node[key] === 'number') return node[key];
-  if (typeof node[key] === 'symbol') return `'${node[key]}'`;
-  if (node[key] === null) return 'null';
-  if (node[key] === undefined) return 'undefined';
-
-  throw new Error(`Unknown type of value: \n${JSON.stringify(node[key])}`);
+const stringify = (value) => {
+  if (value instanceof Object) return '[complex value]';
+  if (typeof value === 'string') return `'${value}'`;
+  return value;
 };
 
-const getNodesRecursive = (nodeList, depth = 0, path = []) => nodeList.map((node) => {
-  switch (node.state) {
-    case ('shared'): {
-      return Array.isArray(node.children)
-        ? getNodesRecursive(node.children, depth + 1, [...path, node.key]).join('')
-        : '';
-    }
-    case ('added'): {
-      return [
-        `Property '${[...path, node.key].join('.')}' was added`,
-        Array.isArray(node.children)
-          ? `with value: ${getValueString(node, 'children')}\n`
-          : `with value: ${getValueString(node, 'newVal')}\n`,
-      ].join(' ');
-    }
-    case ('deleted'): {
-      return `Property '${[...path, node.key].join('.')}' was removed\n`;
-    }
-    case ('updated'): {
-      return [
-        `Property '${[...path, node.key].join('.')}' was updated.`,
-        Object.hasOwn(node, 'oldVal')
-          ? `From ${getValueString(node, 'oldVal')}`
-          : `From ${getValueString(node, 'children')}`,
-        Object.hasOwn(node, 'newVal')
-          ? `to ${getValueString(node, 'newVal')}\n`
-          : `to ${getValueString(node, 'children')}\n`,
-      ].join(' ');
-    }
-    default: throw new Error(`Wrong state: \n${JSON.stringify(node)}`);
-  }
-});
-
 export default (diff) => {
-  const nodes = getNodesRecursive(diff).join('').slice(0, -1);
-  return `${nodes}`;
+  const recursion = (nodeList, path = []) => nodeList.map((node) => {
+    switch (node.type) {
+      case 'added': return [
+        `Property '${[...path, node.key].join('.')}'`,
+        `was added with value: ${stringify(node.value)}`,
+      ].join(' ');
+      case 'deleted': return `Property '${[...path, node.key].join('.')}' was removed`;
+      case 'changed': return [
+        `Property '${[...path, node.key].join('.')}' was updated.`,
+        `From ${stringify(node.value1)}`,
+        `to ${stringify(node.value2)}`,
+      ].join(' ');
+      case 'shared': return null;
+      case 'nested': return `${recursion(node.children, [...path, node.key])}`;
+      default: throw new Error(`Unknown node type: ${node.type}`);
+    }
+  }).filter((row) => row).join('\n');
+  return `${recursion(diff).trim()}`;
 };
